bf7deb713b08e49161fb92d327afce546fb96b6d
marcus.conduru <marcus.conduru@gmail.com>
2023-09-22 21:12:18 -0300

    feat: ensure add input component

---
+++ src/presetation/components/input/input-styles.tsx
@@ -0,0 +1,12 @@
+import styled from 'styled-components/native';
+
+export const InputContainer = styled.TextInput`
+  background-color: #ff85a8;
+  padding: 10px 20px;
+  height: 40px;
+  margin: 12px;
+  font-size: 15px;
+  color: #fff;
+  border-radius: 20px;
+  width: 90vw;
+`;
---
+++ src/presetation/components/input/input.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import {InputContainer} from './input-styles';
+import {Dimensions} from 'react-native';
+
+type Props = {
+  value: string;
+  change: React.Dispatch<React.SetStateAction<any>>;
+  secure?: boolean;
+};
+
+const Inputs: React.FC<Props> = ({change, value, secure = false}: Props) => {
+  return (
+    <InputContainer
+      secureTextEntry={secure}
+      style={{width: Dimensions.get('window').width - 60}}
+      onChangeText={change}
+      value={value}
+    />
+  );
+};
+
+export default Inputs;
