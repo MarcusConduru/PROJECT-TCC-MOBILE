435de17e1c2dbcdc68404967469009d1797a645a
marcus.conduru <marcus.conduru@gmail.com>
2023-09-28 20:35:55 -0300

    feat: ensure add signup page

---
+++ src/presetation/pages/signup/signup-styles.tsx
@@ -0,0 +1,20 @@
+import styled from 'styled-components/native';
+
+export const SignupContainer = styled.View`
+  flex: 1;
+  background-color: #f75683;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const SignupImage = styled.Image`
+  height: 250px;
+  object-fit: contain;
+  margin-top: -80px;
+`;
+
+export const SignupIcon = styled.TouchableOpacity`
+  position: absolute;
+  top: 25px;
+  left: 15px;
+`;
---
+++ src/presetation/pages/signup/signup.tsx
@@ -0,0 +1,47 @@
+import React, {useState} from 'react';
+
+import {View} from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome5';
+import {Button, Input, Label} from '../../components';
+import dogImage from '../../../img/HPcachorro.png';
+import {SignupContainer, SignupIcon, SignupImage} from './signup-styles';
+import {useNavigation} from '@react-navigation/native';
+
+const Signup: React.FC = () => {
+  const [email, setEmaill] = useState('');
+  const [Password, setPassword] = useState('');
+  const navigator = useNavigation();
+
+  const SignupAuthentication = () => {
+    return;
+  };
+
+  return (
+    <SignupContainer>
+      <SignupIcon onPress={navigator.goBack}>
+        <Icon name="arrow-left" size={25} color={'#fff'} />
+      </SignupIcon>
+
+      <SignupImage source={dogImage} />
+
+      <View>
+        <Label name="Email" />
+        <Input value={email} change={setEmaill} />
+      </View>
+
+      <View>
+        <Label name="Password" />
+        <Input secure value={Password} change={setPassword} />
+      </View>
+
+      <Button
+        click={SignupAuthentication}
+        name="Cadastrar"
+        email={email}
+        password={Password}
+      />
+    </SignupContainer>
+  );
+};
+
+export default Signup;
