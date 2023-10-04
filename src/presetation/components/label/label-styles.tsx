1dc3db7fdfaa744f08208c0bdcf11ac961ba23f5
marcus.conduru <marcus.conduru@gmail.com>
2023-10-03 21:31:05 -0300

    chore: add font lato in project

--- src/presetation/components/label/label-styles.tsx
+++ src/presetation/components/label/label-styles.tsx
@@ -1,7 +1,7 @@
 import styled from 'styled-components/native';
 
 export const LabelContainer = styled.Text`
-  font-family: 'Inter_500Medium';
-  color: #fff;
+  font-family: 'Lato-Regular';
+  color: #000;
   font-size: 16px;
 `;
