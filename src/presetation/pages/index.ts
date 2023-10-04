30a0393087a235cd1dfe9906b06214a3aa89111a
marcus.conduru <marcus.conduru@gmail.com>
2023-10-03 21:32:33 -0300

    feat: ensure add page list-map

--- src/presetation/pages/index.ts
+++ src/presetation/pages/index.ts
@@ -2,3 +2,6 @@ export {default as Login} from '../pages/login/login';
 export {default as Signup} from '../pages/signup/signup';
 export {default as ProtectionLaw} from '../pages/protection-law/Protection-law';
 export {default as MapSeach} from '../pages/map-seach/map-seach';
+export {default as SelectMap} from '../pages/select-map/select-map';
+export {default as CreateDenunciation} from '../pages/create-denunciation/create-denunciation';
+export {default as ListMap} from '../pages/list-map/list-map';
