79f19ab2fcc4fa2c57e9fcf541b6805e5175bd18
marcus.conduru <marcus.conduru@gmail.com>
2023-10-17 18:44:59 -0300

    feat: ensure add list-map page

--- src/presetation/pages/list-map/list-map-styles.tsx
+++ src/presetation/pages/list-map/list-map-styles.tsx
@@ -1,6 +1,48 @@
 import {GestureHandlerRootView} from 'react-native-gesture-handler';
 import styled from 'styled-components/native';
 
-export const ListContainer = styled.View``;
+export const ListContainer = styled.View`
+  flex: 1;
+  background-color: #fff;
+`;
 
 export const ListGesture = styled(GestureHandlerRootView)``;
+
+export const ListText = styled.Text`
+  color: #888;
+  font-size: 18px;
+  margin-top: 10px;
+  font-family: 'Lato-Regular';
+`;
+
+export const ListTitle = styled.Text`
+  color: #000;
+  font-size: 28px;
+  margin-top: 10px;
+  font-family: 'Lato-Bold';
+  margin-bottom: 10px;
+`;
+
+export const ListSub = styled.Text`
+  color: #000;
+  font-size: 20px;
+  margin-top: 10px;
+  font-family: 'Lato-Bold';
+`;
+
+export const ListView = styled.View`
+  padding: 5px;
+`;
+
+export const ListDog = styled.View`
+  flex: 1;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const ListAlert = styled.Text`
+  font-size: 20px;
+  color: #000;
+  font-family: 'Lato-Bold';
+  margin-top: 15px;
+`;
--- src/presetation/pages/list-map/list-map.tsx
+++ src/presetation/pages/list-map/list-map.tsx
@@ -1,11 +1,17 @@
-/* eslint-disable react-native/no-inline-styles */
 import React from 'react';
-import {ListContainer, ListGesture} from './list-map-styles';
+import {
+  ListAlert,
+  ListContainer,
+  ListDog,
+  ListGesture,
+  ListSub,
+  ListTitle,
+  ListView,
+} from './list-map-styles';
 import {FlatList} from 'react-native';
-import Swipeable from 'react-native-gesture-handler/Swipeable';
-import {RectButton} from 'react-native-gesture-handler';
+import {ListMapItem} from './component';
+import {Separator} from '../../components';
 import Icon from 'react-native-vector-icons/FontAwesome5';
-import {Text} from 'react-native-elements';
 
 const ListMap: React.FC = () => {
   const listUser = [
@@ -15,33 +21,42 @@ const ListMap: React.FC = () => {
       longitude: -40.4952273,
       title: 'Teste',
     },
+    {
+      key: '2',
+      latitude: -9.4019328,
+      longitude: -40.4952273,
+      title: 'Teste2',
+    },
   ];
 
   return (
     <ListContainer>
-      <ListGesture>
-        <FlatList
-          data={listUser}
-          keyExtractor={item => item.key}
-          renderItem={({item}) => (
-            <Swipeable
-              containerStyle={{borderRightWidth: 5, borderRightColor: 'red'}}
-              renderRightActions={() => (
-                <RectButton onPress={() => {}}>
-                  <Icon name="trash-alt" size={24} color={'#fff'} />
-                </RectButton>
-              )}>
-              <RectButton onPress={() => {}}>
-                <Text>{item.title}</Text>
-              </RectButton>
-            </Swipeable>
-          )}
-        />
-      </ListGesture>
-      {/* <View style={{alignItems: 'center'}}>
-        <MaterialIcons name="pets" size={80} color="#888" />
-        <Text style={styles.text}>Você ainda não tem denúncia</Text>
-      </View> */}
+      <ListView>
+        <ListTitle>Marcações</ListTitle>
+        <ListSub>Todas as suas marcações de denúncias</ListSub>
+      </ListView>
+      {listUser ? (
+        <ListGesture>
+          <FlatList
+            data={listUser}
+            keyExtractor={item => item.key}
+            renderItem={({item}) => (
+              <ListMapItem
+                latitude={item.latitude}
+                longitude={item.longitude}
+                title={item.title}
+                id={item.key}
+              />
+            )}
+            ItemSeparatorComponent={Separator}
+          />
+        </ListGesture>
+      ) : (
+        <ListDog>
+          <Icon name="dog" size={120} color={'#000'} />
+          <ListAlert>Você ainda não tem denúncia</ListAlert>
+        </ListDog>
+      )}
     </ListContainer>
   );
 };
