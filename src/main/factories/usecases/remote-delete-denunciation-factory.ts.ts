c1121d02faf02968c9b772fb81346a9eaa1a02f3
marcus.conduru <marcus.conduru@gmail.com>
2023-10-25 22:09:03 -0300

    feat: ensure add delete-denunciation firabase function

--- src/main/factories/usecases/remote-delete-denunciation-factory.ts.ts
+++ src/main/factories/usecases/remote-delete-denunciation-factory.ts.ts
@@ -1,5 +1,5 @@
+import {RemoteDeleteDenunciation} from '../../../data/usecases';
 import {DeleteDenunciation} from '../../../domain/usecases';
-import {RemoteDeleteDenunciation} from '../../../data/usecases/remote-delete-denunciation';
 
 export const MakeRemoteDeleteDenunciation = (): DeleteDenunciation => {
   return new RemoteDeleteDenunciation();
