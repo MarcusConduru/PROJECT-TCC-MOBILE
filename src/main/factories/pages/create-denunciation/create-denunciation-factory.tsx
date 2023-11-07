a0bae503974b169026905032a80a57ce54c53dee
marcus.conduru <marcus.conduru@gmail.com>
2023-11-07 19:55:53 -0300

    refactor: move pages to external paste

---
+++ src/main/factories/pages/create-denunciation/create-denunciation-factory.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import {CreateDenunciation} from '../../../../presetation/pages';
+import {MakeRemoteAddDenunciation} from '../../usecases';
+
+export const MakeCreateDenunciation: React.FC = () => {
+  return <CreateDenunciation addDenunciation={MakeRemoteAddDenunciation()} />;
+};
---
+++ src/main/factories/pages/list-map/list-map-factory.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import {ListMap} from '../../../../presetation/pages';
+import {useVisible} from '../../../../presetation/hooks';
+import {
+  MakeRemoteDeleteDenunciation,
+  MakeRemoteDeleteImage,
+  MakeRemoteDeleteMessage,
+  MakeRemoteListImage,
+} from '../../usecases';
+
+export const MakeListMap: React.FC = () => {
+  return (
+    <ListMap
+      visibleDenunciation={useVisible()}
+      deleteDenunciation={MakeRemoteDeleteDenunciation()}
+      deleteImage={MakeRemoteDeleteImage()}
+      listImage={MakeRemoteListImage()}
+      deleteMessage={MakeRemoteDeleteMessage()}
+    />
+  );
+};
