f49323b0b3e54f0cbc9b73cd09af46c296ecc016
marcus.conduru <marcus.conduru@gmail.com>
2023-11-07 19:59:22 -0300

    refactor: move validation to Input component

--- src/presetation/components/input/input-styles.tsx
+++ src/presetation/components/input/input-styles.tsx
@@ -1,13 +1,18 @@
 import styled from 'styled-components/native';
 
 export const InputContainer = styled.TextInput`
-  background-color: #ff9cb8;
   padding: 10px 20px;
   height: 40px;
   margin: 12px;
   font-size: 15px;
   color: #000;
   border-radius: 20px;
-  width: 90vw;
   font-family: 'Lato-Regular';
+`;
+
+export const InputText = styled.Text`
+  color: white;
+  font-family: 'Lato-Bold';
+  font-size: 16px;
+  margin: -5px 12px 10px;
 `;
--- src/presetation/components/input/input.tsx
+++ src/presetation/components/input/input.tsx
@@ -1,21 +1,38 @@
+/* eslint-disable react-native/no-inline-styles */
 import React from 'react';
-import {InputContainer} from './input-styles';
-import {Dimensions} from 'react-native';
+import {InputContainer, InputText} from './input-styles';
+import {Dimensions, View} from 'react-native';
+import Label from '../label/label';
 
 type Props = {
   value: string;
   change: React.Dispatch<React.SetStateAction<any>>;
   secure?: boolean;
+  error: string;
+  name: string;
 };
 
-const Input: React.FC<Props> = ({change, value, secure = false}: Props) => {
+const Input: React.FC<Props> = ({
+  change,
+  value,
+  secure = false,
+  error,
+  name,
+}: Props) => {
   return (
-    <InputContainer
-      secureTextEntry={secure}
-      style={{width: Dimensions.get('window').width - 60}}
-      onChangeText={change}
-      value={value}
-    />
+    <View>
+      <Label name={name} />
+      <InputContainer
+        secureTextEntry={secure}
+        style={{
+          width: Dimensions.get('window').width - 60,
+          backgroundColor: error ? '#ff9cb8' : '#fff',
+        }}
+        onChangeText={change}
+        value={value}
+      />
+      {error && <InputText>{error}</InputText>}
+    </View>
   );
 };
 
